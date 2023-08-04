66feb243c96b0236642a53bf6a82b989842104f0
cosmasemerah <emerahcosmas@gmail.com>
2023-08-05 00:49:05 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import path from 'path'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,4 +14,7 @@ export default defineConfig({
       '@db': path.resolve(__dirname, 'src/db'),
     },
   },
-})
+  build: {
+    target: 'esnext',
+  },
+});
